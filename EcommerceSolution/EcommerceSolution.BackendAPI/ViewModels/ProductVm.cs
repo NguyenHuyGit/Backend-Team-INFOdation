9e155e01117bc468f423e50c0cc9b0f3505c2cb1
DHoangDev <hoangntu56@gmail.com>
2022-04-24 18:34:00 +0700

    Get product details API

--- EcommerceSolution/EcommerceSolution.BackendAPI/ViewModels/ProductVm.cs
+++ EcommerceSolution/EcommerceSolution.BackendAPI/ViewModels/ProductVm.cs
@@ -15,7 +15,7 @@ namespace EcommerceSolution.BackendAPI.ViewModels.Product
         public string UserCreate { get; set; }
         public DateTime CreateDate { get; set; }
         public string UserUpdate { get; set; }
-        public DateTime UpdateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
         public int? CategoryId { get; set; }
     }
 }
