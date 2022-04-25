d2d75d3a83133ce806cc70deed6be4aa9b92484c
Nguyenvanhung0147 <63783442+Nguyenvanhung0147@users.noreply.github.com>
2022-04-25 17:02:17 +0700

    Update product

--- EcommerceSolution/EcommerceSolution.BackendAPI/Controllers/ProductController.cs
+++ EcommerceSolution/EcommerceSolution.BackendAPI/Controllers/ProductController.cs
@@ -30,23 +30,20 @@ namespace EcommerceSolution.BackendAPI.Controllers
             var result = await _productService.GetProductList(request);
             return Ok(result);
         }
-        
-        [HttpPost]
-        public async Task<IActionResult> CreateProduct([FromForm] ProductCreateRequest request)
+        [HttpPut]
+        public async Task<IActionResult> ProductUpdate([FromForm]string UserUpdate, ProductUpdate request)
         {
-            if(!ModelState.IsValid)
+            var n = User.FindFirstValue(ClaimTypes.GivenName);
+            var result = await _productService.UpdateProductById(request, n);
+            if (result.IsSuccessed)
             {
-                return BadRequest();
+                return Ok(result);
             }
+            else return BadRequest(result); 
 
-            var userCreate = User.FindFirstValue(ClaimTypes.GivenName);
-            var result = await _productService.CreateProduct(request, userCreate);
-            if(!result.IsSuccessed)
-            {
-                return BadRequest(result);
-            }
-            return Ok(result);
         }
-        
+
+
+
     }
 }
